1a1eaf8de6e492ffd80887fbb68ff31f61396526
notghettolenny <79231993+notghettolenny@users.noreply.github.com>
2022-06-30 01:24:26 +0100

    Add files via upload

---
+++ CoinChange.java
@@ -0,0 +1,23 @@
+package leetcode;
+
+import java.io.BufferedReader;
+import java.io.PrintStream;
+
+import org.omg.SendingContext.RunTime;
+
+public class CoinChange {
+	public int coinChange(int[] coins, int amount) {
+		int result[] = new int[amount];
+		if(coins.length<=0){
+			return -1;
+		}
+		for (int i = 0; i < coins.length; i++) {
+			for (int j = 0; j < result.length; j++) {
+				
+			}
+			System.out.println();
+		}
+		
+		return 0;
+	}
+}
